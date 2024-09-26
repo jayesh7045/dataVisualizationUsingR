239a0dbf8a5112aeac81a9c90ac88a0489dea957
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-09-26 21:54:45 +0530

    understand Vectors in R

---
+++ 2_Vectors.R
@@ -0,0 +1,20 @@
+a<- 1:10
+print(a)
+ b<- c("a", "b")
+ print(b)
+ 
+print((seq(10, 50, by=5)))  # 10, 15, 20, 25, 30, 35, 40, 45, 50
+
+one<-c(1, 2, 3)
+two<-c(4, 5, 6)
+add <- one+two
+print(add)
+
+sub<-one-two
+print(sub)
+
+mul <- one*two
+print(mul)
+
+div <- two/one
+print(div)
