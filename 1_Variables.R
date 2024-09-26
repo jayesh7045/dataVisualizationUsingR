43cd1ba0bb9263a0363e6aa0705c594c0cc30b91
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-09-26 21:53:56 +0530

    understand Variables in R

---
+++ 1_Variables.R
@@ -0,0 +1,38 @@
+a<-10
+print(a)
+c<- c(1, 2, 3)
+print(c)
+
+d<-"Apple"
+print(d)
+
+# Reserved Words in R Language
+# 1) NA -> Not Available, this is used to represent the missing values
+# 2) NAN -> Not a number
+# 3) null -> Used to represent the missing value or undefined value
+# 4) If, else, repeat, while, function, for -> Looping statements
+# 5) Variable name can not start with a digit
+# 6) Variable names can start with a dot
+# 7) NOT start with a _
+
+
+# Data types in R programming language
+# 1) Numeric -> 9.23, 234, 18
+# 2) Integer -> 67L, 34L
+# 3) Complex -> a+bi
+# 4) Logical -> boolean -> true, false
+# 5) charecter -> 'a', 'v'
+
+
+a <- 90
+print(a)
+class(a)
+ b<-'j'
+ 
+class(b)
+rm(a)  # To remove a particular variable from the enviornment
+
+
+ls()
+f<- TRUE
+print(f)
